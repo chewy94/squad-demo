42d4c082791f87195ee8118f8979168f28ab386f
Sean Kimball <smkimbal@gmail.com>
2019-11-13 22:06:39 -0700

    Api working on heroku

--- api/index.js
+++ api/index.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const serverless = require('serverless-http')
 const port = 3000
 
 app.use(cors())
 
 app.get('/user', (req, res) => res.send('Chloe Ku'))
 
-app.listen(process.env.PORT || port, () => console.log(`App is running on port ${port}`))
-
-module.exports.handler = serverless(app)
+app.listen(process.env.PORT || port, () => console.log(`App is running on port ${process.env.PORT || port}`))
