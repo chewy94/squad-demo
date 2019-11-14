64f7a277f6904f39be34bec9715b5f9197123a67
Sean Kimball <smkimbal@gmail.com>
2019-11-13 21:37:07 -0700

    Initial shot at getting netlify functions working

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const serverless = require('serverless-http')
 const port = 3000
 
 app.use(cors())
@@ -8,3 +9,5 @@ app.use(cors())
 app.get('/user', (req, res) => res.send('Chloe Ku'))
 
 app.listen(port, () => console.log(`App is running on port ${port}`))
+
+module.exports.handler = serverless(app)
