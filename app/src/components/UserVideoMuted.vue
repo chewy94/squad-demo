d0eaa1e0621ffe4d36f0a681e3923e94867bca41
Sean Kimball <smkimbal@gmail.com>
2019-11-13 20:11:14 -0700

    Adjust muted sizing to fit all video card sizes

--- app/src/components/UserVideoMuted.vue
+++ app/src/components/UserVideoMuted.vue
@@ -2,12 +2,12 @@
   <v-card flat tile color="#7610cf" height="100%" class="text-center">
     <v-row align="center" style="height: 100%">
       <v-col>
-        <v-card-text>
-          <v-avatar size="78" style="border: 3px solid white;">
+        <v-card-text class="pb-1">
+          <v-avatar size="48" style="border: 3px solid white;">
             <v-img :src="require('@/assets/squad-avatar.png')"></v-img>
           </v-avatar>
         </v-card-text>
-        <v-card-text class="white--text pt-0">
+        <v-card-text class="white--text pt-0" style="font-size: 12px;">
           <span class="font-weight-black">Simon Nova</span> is paused
         </v-card-text>
       </v-col>
